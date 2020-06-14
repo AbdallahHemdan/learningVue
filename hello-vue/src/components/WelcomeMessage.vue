f85e3e18e8da2588d5ef30c50b479e007e6d9fa0
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-06-14 21:36:59 +0200

    Single File Components

---
+++ hello-vue/src/components/WelcomeMessage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="welcome-message">
+    <h1>Welcome to Instabug</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WelcomeMessage"
+};
+</script>
+
+<style scoped lang="scss">
+$fontSize: 60px;
+h1 {
+  color: red;
+  font-size: $fontSize;
+}
+</style>
--- hello-vue/src/views/About.vue
+++ hello-vue/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <WelcomeMessage />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import WelcomeMessage from "@/components/WelcomeMessage.vue";
+
+export default {
+  name: "About",
+  components: {
+    WelcomeMessage
+  }
+};
+</script>
