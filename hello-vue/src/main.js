8d1f5b4a19ca23ac03f416991f3d7b266adac64b
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-06-15 03:47:05 +0200

    Adding bootstrap and dependencies

--- hello-vue/src/main.js
+++ hello-vue/src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./scss/main.scss";
 import "normalize.css";
 
--- hello-vue/src/views/About.vue
+++ hello-vue/src/views/About.vue
@@ -2,6 +2,20 @@
   <div class="about">
     <h1>{{ pageName }}</h1>
     <p>{{ pageDescription }}</p>
+    <div class="container">
+      <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Holy guacamole!</strong> You should check in on some of those
+        fields below.
+        <button
+          type="button"
+          class="close"
+          data-dismiss="alert"
+          aria-label="Close"
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
