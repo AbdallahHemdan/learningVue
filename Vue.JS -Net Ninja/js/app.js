d6aaf312c88d4ada2b9e71135e8650106c16b36a
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-06-17 06:05:02 +0200

    Simple Punchbag Game

--- Vue.JS -Net Ninja/js/app.js
+++ Vue.JS -Net Ninja/js/app.js
@@ -1,21 +1,26 @@
 let vm = new Vue({
   el: "#root",
   data: {
-    persons: [
-      {
-        name: "Abdallah",
-        age: 21,
-      },
-      {
-        name: "Ahmed",
-        age: 47,
-      },
-      {
-        name: "Omar",
-        age: 11,
-      },
-    ],
+    health: 100,
+    isEndGame: false,
   },
-  methods: {},
-  computed: {},
+  methods: {
+    punchBag: function () {
+      this.health -= 10;
+      if (this.health <= 0) {
+        // you dead
+        this.isEndGame = true;
+        this.health;
+      }
+    },
+    resetHealth: function () {
+      this.health = 100;
+      this.isEndGame = false;
+    },
+  },
+  computed: {
+    scoreStyle: function () {
+      return { width: `${this.health}%` };
+    },
+  },
 });
