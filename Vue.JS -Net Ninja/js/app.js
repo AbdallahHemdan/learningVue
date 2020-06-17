b49f149df31f3453928b16278396900350bb9e4f
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-06-17 02:13:58 +0200

    Dynamic CSS Classes

--- Vue.JS -Net Ninja/js/app.js
+++ Vue.JS -Net Ninja/js/app.js
@@ -1,26 +1,21 @@
 let vm = new Vue({
   el: "#root",
   data: {
-    age: 25,
-    a: 0,
-    b: 0,
+    isBrand: true,
+    isTitle: true,
+    isAvailable: false,
   },
   methods: {
-    incrementA: function () {
-      this.a++;
-    },
-    incrementB: function () {
-      this.b++;
+    toggleAvailability: function () {
+      this.isAvailable ^= 1;
     },
   },
   computed: {
-    addAgeToA: function () {
-      console.log("addAgeToA");
-      return this.age + this.a;
-    },
-    addAgeToB: function () {
-      console.log("addAgeToB");
-      return this.age + this.b;
+    productClasses: function () {
+      return {
+        available: this.isAvailable,
+        outOfStore: !this.isAvailable,
+      };
     },
   },
 });
