b3b388b660ac6c0bc382145258b2209258defa33
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-06-18 05:13:42 +0200

    Vue CLI, Vue Files and The Root Component

---
+++ Vue.JS -Net Ninja/with CLI/vue-ninja/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h1>Hello, {{ name }}</h1>
+    <p>{{ title }}</p>
+    <p>{{ greeting() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data: function() {
+    return {
+      name: "Hemdan",
+      title: "my name is Abdallah ahmed hemdan"
+    };
+  },
+  methods: {
+    greeting: function() {
+      return `hey, ${this.name}`;
+    }
+  },
+  components: {}
+};
+</script>
