40982ed5d6c2fee8e994af033110971fcafb15c9
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-06-18 04:02:18 +0200

    Add Refs

--- Vue.JS -Net Ninja/js/app.js
+++ Vue.JS -Net Ninja/js/app.js
@@ -1,31 +1,15 @@
-Vue.component("Greeting", {
-  template: `
-    <div>
-      <p>Hello, I am a reusable component {{name}}</p>
-      <button @click="changeMyFirstName">Click me please</button>
-    </div>
-  `,
-  data: function () {
-    return {
-      name: "Abdallah",
-    };
-  },
-  methods: {
-    changeMyFirstName: function () {
-      this.name = "Hemdan";
-    },
-  },
-});
 let vm = new Vue({
   el: "#root",
-  data: {},
-  methods: {},
-  computed: {},
-});
-
-let vm2 = new Vue({
-  el: "#root2",
-  data: {},
-  methods: {},
+  data: {
+    firstName: "",
+    emailAddress: "",
+  },
+  methods: {
+    getRefs: function () {
+      this.firstName = this.$refs.firstName.value;
+      this.emailAddress = this.$refs.email.value;
+      console.log(this.$refs);
+    },
+  },
   computed: {},
 });
