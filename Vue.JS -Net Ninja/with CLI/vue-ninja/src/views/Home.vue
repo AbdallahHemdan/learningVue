2a455fa123627f262276f27498d5a0df8ec81145
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-06-19 10:02:47 +0200

    Dynamic Components

--- Vue.JS -Net Ninja/with CLI/vue-ninja/src/views/Home.vue
+++ Vue.JS -Net Ninja/with CLI/vue-ninja/src/views/Home.vue
@@ -1,47 +1,28 @@
 <template>
   <div class="">
-    <form-helper>
-      <div slot="form-header">
-        <h1>I am the form header</h1>
-      </div>
-      <div slot="form-fields">
-        <input type="text" placeholder="username" required v-model="username" />
-        <input type="email" placeholder="email" required v-model="email" />
-        <input
-          type="password"
-          placeholder="password"
-          required
-          v-model="password"
-        />
-      </div>
-      <div slot="form-controls">
-        <button type="submit" @click.prevent="handleSubmit">Sumit</button>
-      </div>
-    </form-helper>
+    <keep-alive>
+      <component :is="component"></component>
+    </keep-alive>
+    <button @click="component = 'form-one'">first form</button>
+    <button @click="component = 'form-two'">second form</button>
   </div>
 </template>
 
 <script>
-import FormHelper from "./../components/FormHelper";
+import formOne from "./../components/formOne";
+import formTwo from "./../components/formTwo";
 
 export default {
   name: "Home",
   components: {
-    "form-helper": FormHelper
+    "form-one": formOne,
+    "form-two": formTwo
   },
   data: function() {
     return {
-      username: "",
-      email: "",
-      password: ""
+      component: "form-two"
     };
   },
-  methods: {
-    handleSubmit: function() {
-      console.log(this.username);
-      console.log(this.email);
-      console.log(this.password);
-    }
-  }
+  methods: {}
 };
 </script>
