a7b5ede5f0f0d0ea76b013bf734d2c2289bde866
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-06-23 20:20:19 +0200

    #3 - Setting up a Central Store

--- Vuex Tutorial-Net Ninja/vuex/src/App.vue
+++ Vuex Tutorial-Net Ninja/vuex/src/App.vue
@@ -12,14 +12,7 @@ import ProductListTwo from "./components/ProductListTwo";
 export default {
   name: "App",
   data: function() {
-    return {
-      products: [
-        { name: "Banana Skin", price: 20 },
-        { name: "Shiny Star", price: 40 },
-        { name: "Green Shells", price: 60 },
-        { name: "Red Shells", price: 80 }
-      ]
-    };
+    return {};
   },
   components: {
     "product-list-one": ProductListOne,
@@ -34,5 +27,6 @@ export default {
 <style scoped>
 #app {
   font-family: monospace;
+  color: #555;
 }
 </style>
--- Vuex Tutorial-Net Ninja/vuex/src/store/index.js
+++ Vuex Tutorial-Net Ninja/vuex/src/store/index.js
@@ -1,15 +1,18 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    products: [
+      { name: "Banana Skin", price: 20 },
+      { name: "Shiny Star", price: 40 },
+      { name: "Green Shells", price: 60 },
+      { name: "Red Shells", price: 80 },
+    ],
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+});
